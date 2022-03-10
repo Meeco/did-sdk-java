287dd49b3a319404619634ded2600ceace8f3341
elena <elena.murova@meeco.me>
2022-03-10 14:23:37 +1100

    fix: Fix PMD rule violations and update jszip library. Update javadocs libs.

--- src/main/java/com/hedera/hashgraph/identity/hcs/MessageTransaction.java
+++ src/main/java/com/hedera/hashgraph/identity/hcs/MessageTransaction.java
@@ -2,16 +2,23 @@ package com.hedera.hashgraph.identity.hcs;
 
 import com.google.common.base.Strings;
 import com.hedera.hashgraph.identity.utils.Validator;
-import com.hedera.hashgraph.sdk.*;
-import java8.util.function.BiFunction;
-import org.threeten.bp.Instant;
-
+import com.hedera.hashgraph.sdk.Client;
+import com.hedera.hashgraph.sdk.Hbar;
+import com.hedera.hashgraph.sdk.PrecheckStatusException;
+import com.hedera.hashgraph.sdk.PrivateKey;
+import com.hedera.hashgraph.sdk.TopicId;
+import com.hedera.hashgraph.sdk.TopicMessageSubmitTransaction;
+import com.hedera.hashgraph.sdk.Transaction;
+import com.hedera.hashgraph.sdk.TransactionId;
+import com.hedera.hashgraph.sdk.TransactionResponse;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.concurrent.TimeoutException;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.UnaryOperator;
+import java8.util.function.BiFunction;
+import org.threeten.bp.Instant;
 
 public abstract class MessageTransaction<T extends Message> {
 
