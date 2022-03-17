07f745d7d668e9338fe7f7a2daceb36197bab4c8
elena <elena.murova@meeco.me>
2022-03-17 13:52:37 +1100

    refactor: Add java-multibase and jackson-databind libs, update some events logic.
    Signed-off-by: elena <elena.murova@meeco.me>; Signed-off-by: linasi <l.isganaitis@gmail.com>; Signed-off-by: vijay <vijayshiyani@gmail.com>; Signed-off-by: Elena <43060587+emurova@users.noreply.github.com>; Signed-off-by: Linas Išganaitis <l.isganaitis@gmail.com>; Signed-off-by: Vijay Shiyani <vshiyani@meeco.me>; Signed-off-by: Derek Munneke (Meeco) <derek.munneke@meeco.me>

--- src/main/java/com/hedera/hashgraph/identity/hcs/did/event/owner/HcsDidCreateDidOwnerEvent.java
+++ src/main/java/com/hedera/hashgraph/identity/hcs/did/event/owner/HcsDidCreateDidOwnerEvent.java
@@ -1,16 +1,55 @@
 package com.hedera.hashgraph.identity.hcs.did.event.owner;
 
+import com.hedera.hashgraph.identity.DidError;
 import com.hedera.hashgraph.identity.hcs.did.event.HcsDidEvent;
+import com.hedera.hashgraph.identity.hcs.did.event.HcsDidEventTargetName;
+import com.hedera.hashgraph.sdk.PublicKey;
 
 public class HcsDidCreateDidOwnerEvent extends HcsDidEvent {
+
+    public static String KEY_TYPE = "Ed25519VerificationKey2018";
+
+    public HcsDidEventTargetName targetName = HcsDidEventTargetName.DID_OWNER;
+
+    String id;
+    String type = HcsDidCreateDidOwnerEvent.KEY_TYPE;
+    String controller;
+    PublicKey publicKey;
+
+    HcsDidCreateDidOwnerEvent(String id, String controller, PublicKey publicKey) throws DidError {
+        super();
+
+        if (id.isEmpty() || controller.isEmpty() || publicKey == null) {
+            throw new DidError("Validation failed. DID Owner args are missing");
+        }
+
+        if (!this.isOwnerEventIdValid(id)) {
+            throw new DidError("Event ID is invalid. Expected format: {did}#did-root-key");
+        }
+
+        this.id = id;
+        this.controller = controller;
+        this.publicKey = publicKey;
+    }
     @Override
     protected String getId() {
-        return null;
+        return id;
+    }
+    public String getType() {
+        return type;
+    }
+
+    public String getController() {
+        return controller;
+    }
+
+    public PublicKey getPublicKey() {
+        return publicKey;
     }
 
     @Override
     protected String toJsonTree() {
-        return null;
+        return type;
     }
 
     @Override
