afad35580d3ae270d6e7ffc5439719a6332d7955
Vijay Shiyani <vshiyani@meeco.me>
2022-04-01 16:42:00 +1100

    removed checked in pk
    Signed-off-by: elena <elena.murova@meeco.me>; Signed-off-by: linasi <l.isganaitis@gmail.com>; Signed-off-by: vijay <vijayshiyani@gmail.com>; Signed-off-by: Elena <43060587+emurova@users.noreply.github.com>; Signed-off-by: Linas Išganaitis <l.isganaitis@gmail.com>; Signed-off-by: Vijay Shiyani <vshiyani@meeco.me>; Signed-off-by: Derek Munneke (Meeco) <derek.munneke@meeco.me>

--- lib/src/test/java/com/hedera/hashgraph/identity/hcs/did/HcsDidTest.java
+++ lib/src/test/java/com/hedera/hashgraph/identity/hcs/did/HcsDidTest.java
@@ -1,27 +1,12 @@
 package com.hedera.hashgraph.identity.hcs.did;
 
 import com.hedera.hashgraph.identity.DidError;
-import com.hedera.hashgraph.sdk.AccountId;
-import com.hedera.hashgraph.sdk.Client;
 import com.hedera.hashgraph.sdk.PrivateKey;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 public class HcsDidTest {
-    final AccountId operatorId = AccountId.fromString("0.0.12710106");
-    final PrivateKey operatorKey = PrivateKey.fromString("302e020100300506032b657004220420bc45334a1313725653d3513fcc67edb15f76985f537ca567e2177b0be9906d49");
-    final List<String> mirrorNetworks = List.of("hcs.testnet.mirrornode.hedera.com:5600");
-
-    final Client client = Client.forTestnet();
-
-    HcsDidTest() throws InterruptedException {
-        this.client.setMirrorNetwork(this.mirrorNetworks);
-        this.client.setOperator(this.operatorId, this.operatorKey);
-    }
-
 
     @Test
     @DisplayName("throws error because of missing identifier and privateKey")
