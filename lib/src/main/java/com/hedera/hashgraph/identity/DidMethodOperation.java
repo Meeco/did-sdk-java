7be70043e00837db91f5de38dd680b95d83969e9
Vijay Shiyani <vshiyani@meeco.me>
2022-04-01 15:39:23 +1100

    register did test working

--- lib/src/main/java/com/hedera/hashgraph/identity/DidMethodOperation.java
+++ lib/src/main/java/com/hedera/hashgraph/identity/DidMethodOperation.java
@@ -1,5 +1,7 @@
 package com.hedera.hashgraph.identity;
 
+import java.util.Arrays;
+
 public enum DidMethodOperation {
 
     CREATE("create"),
@@ -12,6 +14,12 @@ public enum DidMethodOperation {
     DidMethodOperation(String label) {
 
         this.label = label;
+    }
+
+    public static DidMethodOperation get(final String typeName) {
+        return Arrays.stream(values())
+                .filter(t -> t.label.equals(typeName)).findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Invalid DID service type name: " + typeName));
     }
 
     @Override
